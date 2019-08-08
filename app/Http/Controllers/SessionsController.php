d0587d0b81ea8cdff99b427b58f1c81a9cdb5db3
esout <ldzredsoul@163.com>
2019-08-08 08:20:55 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,6 +26,13 @@ class SessionsController extends Controller
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
        }
-       
     }
+    
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','你已成功退出');
+        return redirect('login');
+    }
+    
 }
